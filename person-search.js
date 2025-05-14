0375554d4f46bcd7f3a82451d6b30c55d8e07e41
DanWooster <stydw7@nottingham.ac.uk>
2025-05-14 15:29:14 +0100

    Rewrite JS logic for vehicle addition and fix schema column names

--- person-search.js
+++ person-search.js
@@ -1,10 +1,5 @@
 // script.js
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
-
-const supabase = createClient(
-  'https://oxinpfpbpvsglksvpnug.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im94aW5wZnBicHZzZ2xrc3ZwbnVnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM1MTI1MTgsImV4cCI6MjA1OTA4ODUxOH0.MpFlhTkVXe8nqunU_87cZbf8MQdg8ogJqBbRbU0nIxI'
-)
+import supabase from './supabase.js';
 
 // search-a-person page
 const submitPersonBtn = document.getElementById("search-person");
@@ -88,11 +83,3 @@ function displayPeopleResults(people) {
 
   
 }
-
-
-
-
-
-
-
-
--- vehicle-search.js
+++ vehicle-search.js
@@ -1,10 +1,4 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
-
-
-const supabase = createClient(
-  'https://oxinpfpbpvsglksvpnug.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im94aW5wZnBicHZzZ2xrc3ZwbnVnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM1MTI1MTgsImV4cCI6MjA1OTA4ODUxOH0.MpFlhTkVXe8nqunU_87cZbf8MQdg8ogJqBbRbU0nIxI'
-)
+import supabase from './supabase.js';
 
 document.addEventListener("DOMContentLoaded", () => {
   const submitVehicleBtn = document.getElementById("submit");
